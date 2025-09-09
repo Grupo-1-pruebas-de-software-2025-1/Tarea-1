918b21057383550d5de3fb2745189ac76a08e8c4
Francisca Figueroa <fcafigueroal@gmail.com>
2025-09-09 16:57:24 -0300

    feat: se añade devolución de entrada

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,3 @@
-package src;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.InputMismatchException;
@@ -42,6 +40,7 @@ public class Menu {
             switch (opcion) {
                 case 1 -> manejarEventos(scanner);
                 case 3 -> manejarVenta(scanner);
+                case 4 -> manejarRegistrarDevolucion(scanner);
                 default -> {
                     System.out.println("Opción no válida");
                     salir = true;
@@ -69,7 +68,7 @@ public class Menu {
                 case 1 -> manejarCrearEvento(scanner);
                 case 2 -> manejarMostrarEventos();
                 case 3 -> manejarEditarEvento(scanner);
-                case 4 -> manejarEliminarEvento(scanner);
+                case 4 -> manejarRegistrarDevolucion(scanner);
                 case 0 -> volver = true; // Regresa al menú principal
                 default -> System.out.println("Opción no válida, intente de nuevo.");
             }
@@ -92,6 +91,22 @@ public class Menu {
             System.out.println("🔴 Venta no realizada.");
         }
     }      
+
+    private void manejarRegistrarDevolucion(Scanner scanner) {
+        System.out.print("Ingrese el nombre del evento: ");
+        scanner.nextLine(); // limpiar buffer
+        String nombreEvento = scanner.nextLine();
+
+        System.out.print("Ingrese la cantidad de entradas a devolver: ");
+        int cantidad = scanner.nextInt();
+
+        boolean exito = ventaManager.registrarDevolucion(nombreEvento, cantidad);
+        if (exito) {
+            System.out.println("✅ Devolución registrada correctamente.");
+        } else {
+            System.out.println("❌ No se pudo registrar la devolución.");
+        }
+    }
 
 
     private void manejarCrearEvento(Scanner scanner) {
